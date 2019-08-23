f47279a5399ce0730c0e267083d9d7dd7ce25e47
ngchwanlii <ngchwanlii@gmail.com>
2019-08-23 14:24:45 -0700

    Added my page

---
+++ lib/pages/my_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:journey/widget/webview.dart';
+
+const MY_URL = 'https://m.ctrip.com/webapp/myctrip/';
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  final PageController _controller = PageController(
+      initialPage: 0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: WebView(
+            url: MY_URL,
+            hideAppBar: true,
+            backForbid: true,
+            statusBarColor: '4c5bca',
+          ),
+        )
+    );
+  }
+}
